9d9c8715a60f75e3937a700b7dd01ba3ea1c29fa
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-12-19 22:52:35 +0530

    easy-01

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+  
+  let n1 = str1.length;
+  let n2 = str2.length;
+  
+  if(n1!=n2)
+  return false;
 
+  str1.sort();
+  str2.sort();
+
+  for(let i=0;i<str1.length;i++){
+    if(str1[i]!=str2[i])
+    return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
